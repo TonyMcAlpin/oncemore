62f37096cfa7ec27f4357db718ad437d1e2b0fc6
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 14:49:19 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -2,7 +2,7 @@
   class Database {
     // DB Params
     private $host = 'localhost';
-    //private $port ='5432';               //added
+    //private $port ='5432';            
     private $db_name = 'quotes';
     private $username = 'root';          // changed from root to postgres
     private $password = '';          // changed from '' to postgres
@@ -11,7 +11,7 @@
     // DB Connect
     public function connect() {
       $this->conn = null;
-      $dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}"; // added 
+      //$dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
       try { 
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); //$this->conn = new PDO($dsn, $this->username, $this->password);      
