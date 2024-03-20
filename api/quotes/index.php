8ab8ed77d9f3ec8b5fe41115db6d4727dd1e6b89
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 09:16:31 -0600

    Update index.php

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,36 +1,37 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+$endpoint = $_SERVER['REQUEST_URI'];
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-    $endpoint = $_SERVER['REQUEST_URI'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+}
 
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit();
-    }
+try {
+    require_once __DIR__ . '/../../config/Database.php';
+    require_once __DIR__ . '/../../models/Quote.php';
 
-    require_once '../../config/Database.php';
-    require_once '../../models/Quote.php';
-    include_once '../../config/Database.php';  // Maybe
-    include_once '../../models/Quote.php';   //Maybe
-
-    
-
-    
-    
-    switch($method){
+    switch ($method) {
         case 'GET':
-            require '../../api/quotes/read.php';
+            require __DIR__ . '/../../api/quotes/read.php';
             break;
         case 'POST':
-            require '../../api/quotes/create.php';
-            break; 
+            require __DIR__ . '/../../api/quotes/create.php';
+            break;
         case 'PUT':
-            require '../../api/quotes/update.php';
-            break; 
+            require __DIR__ . '/../../api/quotes/update.php';
+            break;
         case 'DELETE':
-            require '../../api/quotes/delete.php';
-            break;  
+            require __DIR__ . '/../../api/quotes/delete.php';
+            break;
+        default:
+            http_response_code(405); // Method Not Allowed
+            echo json_encode(array("message" => "Method not allowed."));
     }
+} catch (Exception $e) {
+    http_response_code(500); // Internal Server Error
+    echo json_encode(array("message" => "Internal server error."));
+}
