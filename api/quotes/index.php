4f050610587a493a19a71dd1188703b44030cc3c
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 12:44:04 -0600

    Update index.php

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -2,12 +2,8 @@
 
     require_once '../../config/Database.php';
     require_once '../../models/Quote.php';
-    //include_once '../../config/Database.php';  // Maybe
-    //include_once '../../models/Category.php';   //Maybe
 
     $method = $_SERVER['REQUEST_METHOD'];
-    $endpoint = $_SERVER['REQUEST_URI'];
-
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     
