0568c848b7080e064659b462384ffd441a0be184
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 15:05:35 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -2,19 +2,19 @@
   class Database {
     // DB Params
     private $host = 'localhost';
-    //private $port ='5432';            
+    private $port ='5432';            
     private $db_name = 'quotes';
-    private $username = 'root';          // changed from root to postgres
-    private $password = '';          // changed from '' to postgres
+    private $username = 'postgres';          // changed from root to postgres
+    private $password = 'postgres';          // changed from '' to postgres
     private $conn; 
 
     // DB Connect
     public function connect() {
       $this->conn = null;
-      //$dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+      $dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); //$this->conn = new PDO($dsn, $this->username, $this->password);      
+        $this->conn = new PDO($dsn, $this->username, $this->password);  //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);     
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
