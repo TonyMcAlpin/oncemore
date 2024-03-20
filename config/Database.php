b63b1fb21afd2ae862959a04043f375460a317e4
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 16:59:56 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,25 +1,43 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
-    private $port ='5432';            
-    private $db_name = 'quotes_u9ab';
-    private $username = 'postgres';          // changed from root to postgres
-    private $password = 'postgres';          // changed from '' to postgres
-    private $conn; 
+    //private $host = 'localhost';
+    //private $port ='5432';            
+    //private $db_name = 'quotes_u9ab';
+    //private $username = 'postgres';          // changed from root to postgres
+    //private $password = 'postgres';          // changed from '' to postgres
+    //private $conn; 
+    private $conn;
+    private $host;
+    private $port;
+    private $dbname;
+    private $username;
+    private $password;
+
+    public function __construct(){
+      $this->username =getenv('USERNAME');
+      $this->password =getenv('PASSWORD');
+      $this->dbname =getenv('DBNAME');
+      $this->host=('HOST');
+      $this->port=('PORT');
+      
+
+    }
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
-      $dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+      if(this->conn){
+        return this->conn;
+      }else{
+        $dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+      
 
-      try { 
-        $this->conn = new PDO($dsn, $this->username, $this->password); //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); //      
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
-      }
-
-      return $this->conn;
+        try { 
+          $this->conn = new PDO($dsn, $this->username, $this->password); //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); //      
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
+        }
     }
   }
+}
