8c2baaa1ac27e33309fb1b62a0e7ff8a7c675a7d
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 13:01:19 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -2,17 +2,19 @@
   class Database {
     // DB Params
     private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
-    private $conn;
+    private $port ='5432';               //added
+    private $db_name = 'quotes';
+    private $username = 'postgres';          // changed from root to postgres
+    private $password = 'postgres';          // changed from '' to postgres
+    private $conn; 
 
     // DB Connect
     public function connect() {
-      $this->conn = "Internal Database URL";
+      $this->conn = null;
+      $dsn= "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}"; // added 
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); //Look at mysql vs postgress
+        $this->conn = new PDO($dsn, $this->username, $this->password);      //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
