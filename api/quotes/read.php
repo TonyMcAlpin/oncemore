479e7d254da5359f6904967508fbcdb2f8359b72
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 09:44:00 -0600

    Update read.php

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,11 +1,6 @@
 
 <?php   
-    //Headers
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-
-    
     include_once '../../config/Database.php';
     include_once '../../models/Quote.php';
 
