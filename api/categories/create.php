20414617f8f61e1c0859d4a756a3a64e5cf07831
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 20:35:06 -0600

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,11 +1,6 @@
 
 <?php
-  // Headers
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: POST');
-  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
-
+  
   include_once '../../config/Database.php';
   include_once '../../models/Category.php';
   // Instantiate DB & connect
@@ -19,14 +14,7 @@
   $data = json_decode(file_get_contents("php://input"));
 
   $category->category = $data->category;
-
+  $quote->id = $data->id;
+  $quote->category = $data->category;
   // Create Category
-  if($category->create()) {
-    echo json_encode(
-      array('message' => 'Category Created')
-    );
-  } else {
-    echo json_encode(
-      array('message' => 'Category Not Created')
-    );
-  }
+  echo json_encode($quote->create());
