e9dd2edc41858e6f2d457f1c395852c43219acd8
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2024-03-20 19:47:37 -0600

    Update create.php

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -2,10 +2,7 @@
 <?php   
     //Headers
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: POST');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    
 
     
     include_once '../../config/Database.php';
